630e978e31a00c7148ef046ef1f08df64615083d
ertugrullberber <ertugrulberber3461@gmail.com>
2022-05-01 12:49:51 +0300

    Initial Commit

---
+++ coffeOrderApp/coffeOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeOrderAppApp.swift
+//  coffeOrderApp
+//
+//  Created by Ertugrul Berber on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct coffeOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
